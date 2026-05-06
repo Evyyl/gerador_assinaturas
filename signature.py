22cdb49356083bf9dba58406cfbd155723dded67
lucas-vasconcelos-pirani-carneiro <lucasvasconcelospiranicarneiro@gmail.com>
2026-05-06 16:58:45 -0300

    feat: adicionando funcao que verifica a assinatura

--- signature.py
+++ signature.py
@@ -13,3 +13,21 @@ def hasher(arquivo,d,n):
     assinatura64 = base64.b64encode(assinaturabytes).decode('utf-8')
     with open (arquivo+".sig","w") as fsig:
         fsig.write(assinatura64)
+        
+def verificar_assinatura(arquivo, assinatura_arquivo, e, n):
+
+    with open(arquivo, "rb") as f:
+        mensagem = f.read()
+
+    hash_atual = hashlib.sha3_256(mensagem).digest()
+    hash_atual_int = int.from_bytes(hash_atual, byteorder='big')
+
+    with open(assinatura_arquivo, "r") as f:
+        linhas = f.readlines()
+
+    assinatura64 = linhas[0].strip()
+    assinatura_bytes = base64.b64decode(assinatura64)
+    assinatura_int = int.from_bytes(assinatura_bytes, byteorder='big')
+    hash_recuperado = pow(assinatura_int, e, n)
+
+    return hash_recuperado == hash_atual_int
