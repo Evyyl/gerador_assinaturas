95c74735b8d9762b0cb10ce4a2c59e3f4dc27779
lucas-vasconcelos-pirani-carneiro <lucasvasconcelospiranicarneiro@gmail.com>
2026-05-06 16:59:22 -0300

    feat: adicionando o teste que verifica se a assinatura é valida

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import keygen
 import signature
 
-primos = keygen.gerarprimos()
-print (primos)
-chaves = keygen.gerarchave(primos[0], primos[1])
-print("\n")
-print(chaves)
-signature.hasher("exemplo.jpeg",chaves[1][0],chaves[1][1])
+p, q = keygen.gerarprimos()
+print ("O valor de p é :\n", p)
+print ("O valor de q é :\n", q)
 
+pub, priv = keygen.gerarchave(p, q)
+e, n = pub
+d, n = priv
+
+print("Chave Pública: \n", pub)
+print("Chave Privada: \n", priv)
+
+signature.hasher("exemplo.jpeg", d, n) # Utiliza a chave privada
+valido = signature.verificar_assinatura("exemplo.jpeg","exemplo.jpeg.sig",e,n) # Utiliza a chave publica
+print("A assinatura é válida ?", valido)
